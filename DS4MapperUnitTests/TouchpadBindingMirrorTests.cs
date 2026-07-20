726d312dfd717441cac04f410513662e258a54f8
HelloHim <121320973+HelloHim@users.noreply.github.com>
2026-07-20 04:39:56 +0100

    feat(touchpad): mirror touch and click binds

---
+++ DS4MapperUnitTests/TouchpadBindingMirrorTests.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DS4MapperTest;
+using DS4MapperTest.ViewModels;
+
+namespace DS4MapperUnitTests
+{
+    [TestClass]
+    public class TouchpadBindingMirrorTests : BindingHelperBase
+    {
+        [TestMethod]
+        public void TouchpadButtons_AreSharedBetweenKeybindAndTouchpadViews()
+        {
+            Profile profile = new Profile();
+            mapper = new TestMapper(profile);
+            AddButtonBinding("LeftPadTouch", "Left Pad Touch");
+            AddButtonBinding("RightPadTouch", "Right Pad Touch");
+            AddButtonBinding("TouchClick", "Touch Click");
+
+            PrepareDefaultLayerForBindingHelper(profile);
+            FillMappingProfileInitialData(profile, null);
+            SyncActionData(profile);
+
+            mapper.EditActionSet = profile.ActionSets[0];
+            mapper.EditLayer = profile.ActionSets[0].ActionLayers[0];
+
+            ProfileEditorTestViewModel vm = new ProfileEditorTestViewModel(
+                mapper,
+                new ProfileEntity("", "TouchpadMirror", InputDeviceType.SteamController),
+                profile);
+            vm.Test();
+
+            CollectionAssert.AreEquivalent(
+                new string[] { "Left Touch", "Right Touch", "Main Click", "Left Click", "Right Click" },
+                vm.TouchpadButtonBindings.Select(item => item.DisplayName).ToArray());
+
+            AssertTouchpadButton(vm, "Left Touch", "LeftPadTouch");
+            AssertTouchpadButton(vm, "Right Touch", "RightPadTouch");
+            AssertTouchpadButton(vm, "Main Click", "TouchClick");
+
+            FaceButtonBindingItem leftClick = AssertTouchpadButton(vm, "Left Click", "LeftPadClick");
+            FaceButtonBindingItem rightClick = AssertTouchpadButton(vm, "Right Click", "RightPadClick");
+            Assert.AreSame(leftClick,
+                vm.TouchpadBindings.First(item => item.BindingName == "LeftTouchpad").TouchpadClickBinding);
+            Assert.AreSame(rightClick,
+                vm.TouchpadBindings.First(item => item.BindingName == "RightTouchpad").TouchpadClickBinding);
+
+            CollectionAssert.DoesNotContain(
+                vm.ExtraButtonBindings.Select(item => item.DisplayName).ToArray(),
+                "Left Touch");
+            CollectionAssert.DoesNotContain(
+                vm.ExtraButtonBindings.Select(item => item.DisplayName).ToArray(),
+                "Right Touch");
+        }
+
+        private void AddButtonBinding(string id, string displayName)
+        {
+            if (mapper.BindingDict.ContainsKey(id))
+            {
+                return;
+            }
+
+            InputBindingMeta meta =
+                new InputBindingMeta(id, displayName, InputBindingMeta.InputControlType.Button);
+            mapper.BindingList.Add(meta);
+            mapper.BindingDict.Add(id, meta);
+        }
+
+        private static void PrepareDefaultLayerForBindingHelper(Profile profile)
+        {
+            foreach (ActionSet set in profile.ActionSets)
+            {
+                foreach (ActionLayer layer in set.ActionLayers)
+                {
+                    layer.actionSetActionDict.Clear();
+                }
+            }
+        }
+
+        private static FaceButtonBindingItem AssertTouchpadButton(
+            ProfileEditorTestViewModel vm, string displayName, string bindingName)
+        {
+            FaceButtonBindingItem item =
+                vm.TouchpadButtonBindings.FirstOrDefault(binding =>
+                    binding.DisplayName == displayName);
+            Assert.IsNotNull(item, $"{displayName} was not present.");
+            Assert.AreEqual(bindingName, item.BindingName);
+            return item;
+        }
+    }
+}
